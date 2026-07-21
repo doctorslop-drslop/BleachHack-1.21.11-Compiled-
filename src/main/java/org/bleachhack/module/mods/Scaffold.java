0d4667c03556c2e049e73bf362fa48ed503a37b8
doctorslop-drslop <nmishenko30072001@gmail.com>
2026-07-21 16:51:19 +0300

    Update Scaffold settings for better HvH NoAC servers

--- src/main/java/org/bleachhack/module/mods/Scaffold.java
+++ src/main/java/org/bleachhack/module/mods/Scaffold.java
@@ -1,11 +1,3 @@
-/*
- * This file is part of the BleachHack distribution (https://github.com/BleachDev/BleachHack/).
- * Copyright (c) 2021 Bleach and contributors.
- *
- * This source code is subject to the terms of the GNU General Public
- * License, version 3. If a copy of the GPL was not distributed with this
- * file, You can obtain one at: https://www.gnu.org/licenses/gpl-3.0.txt
- */
 package org.bleachhack.module.mods;
 
 import com.google.common.collect.Sets;
@@ -40,8 +32,8 @@ public class Scaffold extends Module {
 	public Scaffold() {
 		super("Scaffold", KEY_UNBOUND, ModuleCategory.WORLD, "Places blocks under you.",
 				new SettingMode("Area", "Normal", "3x3", "5x5", "7x7").withDesc("How big of an area to scaffold."),
-				new SettingSlider("BPT", 1, 10, 2, 0).withDesc("Blocks Per Tick, how many blocks to place per tick."),
-				new SettingSlider("Range", 0, 1, 0.3, 1).withDesc("How far to place ahead of you in Normal mode."),
+				new SettingSlider("BPT", 1, 40, 5, 0).withDesc("Blocks Per Tick, how many blocks to place per tick."),
+				new SettingSlider("Range", 0, 3.0, 1.0, 1).withDesc("How far to place ahead of you in Normal mode."),
 				new SettingRotate(false).withDesc("Rotates when placing blocks."),
 				new SettingToggle("LegitPlace", false).withDesc("Only places on sides you can see."),
 				new SettingToggle("Filter", false).withDesc("Filters certain blocks.").withChildren(
@@ -49,9 +41,9 @@ public class Scaffold extends Module {
 						new SettingItemList("Edit Blocks", "Edit Filtered Blocks", i -> i instanceof BlockItem).withDesc("Edit the filtered blocks.")),
 				new SettingToggle("Tower", true).withDesc("Makes scaffolding straight up much easier.").withChildren(
 						new SettingToggle("Legit", false).withDesc("Slower mode that bypasses some anticheats.")),
-				new SettingToggle("AirPlace", false).withDesc("Places blocks in the air without support blocks."),
+				new SettingToggle("AirPlace", true).withDesc("Places blocks in the air without support blocks."),
 				new SettingToggle("SafeWalk", true).withDesc("Prevents you from walking of edges when scaffold is on."),
-				new SettingToggle("NoSwing", false).withDesc("Doesn't swing your hand clientside."),
+				new SettingToggle("NoSwing", true).withDesc("Doesn't swing your hand clientside."),
 				new SettingToggle("EmptyToggle", false).withDesc("Turns off when you run out of blocks."),
 				new SettingToggle("Highlight", false).withDesc("Highlights the blocks you are placing.").withChildren(
 						new SettingColor("Color", 255, 190, 50).withDesc("Color for the block highlight."),
@@ -103,22 +95,25 @@ public class Scaffold extends Module {
 		if (getSetting(6).asToggle().getState()
 				&& InputUtil.isKeyPressed(mc.getWindow(), InputUtil.fromTranslationKey(mc.options.jumpKey.getBoundKeyTranslationKey()).getCode())) {
 
-			if (mc.world.getBlockState(mc.player.getBlockPos().down()).isReplaceable()
-					&& !mc.world.getBlockState(mc.player.getBlockPos().down(2)).isReplaceable()
-					&& mc.player.getVelocity().y > 0) {
-				mc.player.setVelocity(mc.player.getVelocity().x, -0.1, mc.player.getVelocity().z);
-
-				if (!getSetting(6).asToggle().getChild(0).asToggle().getState()) {
+			if (!getSetting(6).asToggle().getChild(0).asToggle().getState()) {
+				if (mc.player.isOnGround()) {
+					mc.player.setVelocity(mc.player.getVelocity().x, 0.42, mc.player.getVelocity().z);
+				} else if (mc.player.getVelocity().y < 0.15) {
+					mc.player.setVelocity(mc.player.getVelocity().x, -0.15, mc.player.getVelocity().z);
+				}
+			} else {
+				if (mc.world.getBlockState(mc.player.getBlockPos().down()).isReplaceable()
+						&& !mc.world.getBlockState(mc.player.getBlockPos().down(2)).isReplaceable()
+						&& mc.player.getVelocity().y > 0) {
+					mc.player.setVelocity(mc.player.getVelocity().x, -0.1, mc.player.getVelocity().z);
+					mc.player.jump();
+				}
+				if (mc.player.isOnGround()) {
 					mc.player.jump();
 				}
 			}
-
-			if (getSetting(6).asToggle().getChild(0).asToggle().getState() && mc.player.isOnGround()) {
-				mc.player.jump();
-			}
 		}
 
-		// Don't bother doing anything if there aren't any blocks to place on
 		if (blocks.stream().noneMatch(b -> WorldUtils.isBlockEmpty(b))) {
 			return;
 		}
@@ -174,7 +169,6 @@ public class Scaffold extends Module {
 		int neededSteps = size * 4;
 		BlockPos currentPos = center;
 		for (int i = 0; i <= neededSteps; i++) {
-			// Do 1 less step on the last side to not overshoot the spiral
 			if (i == neededSteps)
 				step--;
 
