6ebf97246ca1dc1067ac254d600be8307756c838
doctorslop-drslop <nmishenko30072001@gmail.com>
2026-07-21 17:42:43 +0300

    siem improvements + a new mode
    Basically this mode lets u move the camera under ur feet. good for finding bases under the nethers bedrock or seeing things under you when mining under. sm like that. also added a new mixin

--- src/main/java/org/bleachhack/module/mods/BetterCamera.java
+++ src/main/java/org/bleachhack/module/mods/BetterCamera.java
@@ -1,11 +1,3 @@
-/*
- * This file is part of the BleachHack distribution (https://github.com/BleachDev/BleachHack/).
- * Copyright (c) 2021 Bleach and contributors.
- *
- * This source code is subject to the terms of the GNU General Public
- * License, version 3. If a copy of the GPL was not distributed with this
- * file, You can obtain one at: https://www.gnu.org/licenses/gpl-3.0.txt
- */
 package org.bleachhack.module.mods;
 
 import org.bleachhack.module.Module;
@@ -15,12 +7,37 @@ import org.bleachhack.setting.module.SettingToggle;
 
 public class BetterCamera extends Module {
 
+	private static BetterCamera INSTANCE;
+
 	public BetterCamera() {
 		super("BetterCamera", KEY_UNBOUND, ModuleCategory.RENDER, "Improves the 3rd person camera.",
 				new SettingToggle("CameraClip", true).withDesc("Makes the camera clip into walls."),
 				new SettingToggle("Distance", true).withDesc("Sets a custom camera distance.").withChildren(
-						new SettingSlider("Distance", 0.5, 15, 4, 1).withDesc("The desired camera distance.")));
+						new SettingSlider("Distance", 0.5, 15, 4, 1).withDesc("The desired camera distance.")),
+				new SettingToggle("UnderFeet", false).withDesc("Positions the camera under the player's feet.").withChildren(
+						new SettingSlider("Offset", -10.0, 10.0, -1.0, 1).withDesc("Vertical offset from the feet.")));
+		INSTANCE = this;
 	}
 
-	// Logic handled in MixinCamera::clipToSpace
+	public static boolean isActive() {
+		return INSTANCE != null && INSTANCE.isEnabled();
+	}
+
+	public static boolean isDistanceEnabled() {
+		return isActive() && INSTANCE.getSetting(1).asToggle().getState();
+	}
+
+	public static double getDistance() {
+		if (!isDistanceEnabled()) return 4.0;
+		return INSTANCE.getSetting(1).asToggle().getChild(0).asSlider().getValue();
+	}
+
+	public static boolean isUnderFeetEnabled() {
+		return isActive() && INSTANCE.getSetting(2).asToggle().getState();
+	}
+
+	public static double getOffset() {
+		if (!isUnderFeetEnabled()) return 0.0;
+		return INSTANCE.getSetting(2).asToggle().getChild(0).asSlider().getValue();
+	}
 }
